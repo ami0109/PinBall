b1db3ff8a7f0e65a3ce2bf8c0420edb927038516
suzukiami0109 <rfuk27@gmail.com>
2019-01-21 23:03:54 +0900

    PinBall Lesson5課題
    ボールがターゲットに衝突した時に得点を加算して表示する。基礎課題のみです。

---
+++ Assets/BallController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+
+        //シーン中のScoreTextオブジェクトを取得
+        this.ScoreText = GameObject.Find("ScoreText");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+    }
+
+    //スコアを表示するテキスト
+    private GameObject ScoreText;
+
+    //スコア計算用変数
+    public int score = 0;
+
+
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+        
+        //タグによって得点を変える
+        if (other.gameObject.tag == "SmallStarTag" || other.gameObject.tag == "SmallCloudTag")
+        {
+            score += 10;
+        }
+        else if (other.gameObject.tag == "LargeStarTag" || other.gameObject.tag == "LargeCloudTag")
+        {
+            score += 20;
+        }
+
+        //ScoreTextにスコアを表示
+        this.ScoreText.GetComponent<Text>().text = score.ToString();
+
+    }
+
+}
